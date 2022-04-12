c02a0cf5b4c61424f95722e182fdf200756da3f0
Alexander Photis <55098588+aphotis1@users.noreply.github.com>
2022-04-12 11:17:49 -0500

    Start

---
+++ weather-app/app.js
@@ -0,0 +1,12 @@
+console.log("Starting")
+
+setTimeout(() => {
+    console.log("2 Second Tinter")
+}, 2000)
+
+setTimeout(() => {
+    console.log("0 Second Tinter")
+}, 0)
+
+
+console.log("Stopping")
